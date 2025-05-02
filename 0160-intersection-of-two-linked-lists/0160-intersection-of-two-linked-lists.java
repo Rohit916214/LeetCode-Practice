e1323836c956ea2d54301e5997f0417da4374c32
Rohit916214 <128091706+Rohit916214@users.noreply.github.com>
2025-05-02 23:00:57 +0530

    Time: 1 ms (99.93%), Space: 48.6 MB (68.8%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -34,25 +34,81 @@
 // }
 
 
-//Optimized
+//Optimized brute force by using hashing
+
+// public class Solution {
+//     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+
+//         HashSet<ListNode> set = new HashSet<>();
+
+//         while(headA != null){
+//             set.add(headA);
+//             headA = headA.next;
+//         }
+//         while(headB != null){
+//             if(set.contains(headB)){
+//                 return headB;
+//             }
+//             headB = headB.next;
+//         }
+
+//         return null;
+        
+//     }
+// }
+
+
+//third approach by size difference
 
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
 
-        HashSet<ListNode> set = new HashSet<>();
+        int diff = findDifference(headA, headB);
+
+        if(diff < 0){
+
+            while(diff != 0){
+                headB = headB.next;
+                diff++;
+            }
+
+        }
+
+        else{
+            while(diff != 0){
+                headA = headA.next;
+                diff--;
+            }
+        }
+        
 
         while(headA != null){
-            set.add(headA);
-            headA = headA.next;
-        }
-        while(headB != null){
-            if(set.contains(headB)){
+            if(headA == headB){
                 return headB;
             }
+            headA = headA.next;
             headB = headB.next;
         }
 
         return null;
-        
+    }
+
+    public static int findDifference(ListNode headA, ListNode headB){
+
+        int l1 = 0;
+        int l2 = 0;
+
+        while(headA != null || headB != null){
+
+            if(headA != null){
+                l1++;
+                headA = headA.next;
+            }
+            if(headB != null){
+                l2++;
+                headB = headB.next;
+            }
+        }
+        return l1-l2;
     }
 }
