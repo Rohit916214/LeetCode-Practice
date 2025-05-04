13a023e64955f439fd6c68fab0e0b74df1797e25
Rohit916214 <128091706+Rohit916214@users.noreply.github.com>
2025-05-04 17:32:48 +0530

    Time: 11 ms (38.06%), Space: 57.1 MB (39.22%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.java
@@ -0,0 +1,72 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if(head==null || head.next==null){
+            return head;
+        }
+
+        // find mid
+        ListNode slow=head;
+        ListNode fast=head.next;
+
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        ListNode mid=slow.next;
+        slow.next=null;
+
+        //find right part of head and left part of head
+        ListNode newleft=sortList(head);
+        ListNode newright=sortList(mid);
+
+        // merge the newleft and newright
+
+        return merge(newleft,newright);
+    }
+
+    // method of merge newleft and newright
+
+    public ListNode merge(ListNode head1,ListNode head2){
+        // first we create the temporally linkedlist
+
+        ListNode mergedll=new ListNode(-1);
+        ListNode temp=mergedll;
+
+        // now we apply condition
+
+        while(head1!=null && head2 !=null){
+            if(head1.val <head2.val){       
+                temp.next=head1;
+                head1=head1.next;
+                temp=temp.next;
+            }
+            else{
+                temp.next=head2;
+                head2=head2.next;
+                temp=temp.next;
+            }
+        }
+        while(head1!=null){
+            temp.next=head1;
+            head1=head1.next;
+            temp=temp.next;
+        }
+        while(head2!=null){
+            temp.next=head2;
+            head2=head2.next;
+            temp=temp.next;
+        }
+        return mergedll.next;
+
+    }
+}
