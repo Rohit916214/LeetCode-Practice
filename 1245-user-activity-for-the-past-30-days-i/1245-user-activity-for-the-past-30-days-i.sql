73bf4a4d8c26ce29b3ede3b5c1ef623a3b5abeb3
ROHIT KUMAR <128091706+Rohit916214@users.noreply.github.com>
2025-09-05 19:11:05 +0530

    Time: 445 ms (92.35%), Space: 0B (100%) - LeetHub

--- 1245-user-activity-for-the-past-30-days-i/1245-user-activity-for-the-past-30-days-i.sql
+++ 1245-user-activity-for-the-past-30-days-i/1245-user-activity-for-the-past-30-days-i.sql
@@ -4,3 +4,22 @@ SELECT
 FROM Activity
 WHERE activity_date BETWEEN '2019-06-28' AND '2019-07-27'
 GROUP BY activity_date;
+
+
+
+
+
+/*
+
+        | Data Type           | Example Value            | SQL me kaise likhna hai | Notes                                     |
+| ------------------- | ------------------------ | ----------------------- | ----------------------------------------- |
+| **String (text)**   | `Hello`                  | `'Hello'`               | Always single quotes me                   |
+| **Date**            | `2019-07-27`             | `'2019-07-27'`          | Dates bhi string ki tarah treat hote hain |
+| **Datetime**        | `2019-07-27 10:30:00`    | `'2019-07-27 10:30:00'` | Single quotes me likhna zaroori           |
+| **Number (int)**    | `123`                    | `123`                   | Quotes nahi chahiye                       |
+| **Decimal/Float**   | `45.67`                  | `45.67`                 | Quotes nahi chahiye                       |
+| **Boolean** (MySQL) | `1` (true) / `0` (false) | `1` ya `0`              | MySQL me direct likh sakte ho             |
+
+*/
+
+
